c8460a5add6c4c5b517ac866aecf347cc39b27c3
antn-vtkn <321581364+antn-vtkn@users.noreply.github.com>
2026-09-24 16:57:44 +0300

    RCWAupdatePATH is rewritten as a function rather than a script

--- RCWAupdatePATH.m
+++ RCWAupdatePATH.m
@@ -1,5 +1,29 @@
-addpath('material\');
-addpath('RCWA\');
-addpath('shape\');
+function pathstr=RCWAupdatePATH(pathstr0)
+%add required directories to MATLAB's PATH.
+%   The directory parent to the 'RCWA-MATLAB-module\' is expected
+%   as the parameter. The current ('.') and the parent ('..') directories
+%   are checked by default. The function returns the resulting path 
+%   upon success or NaN on failure
 
+if nargin==0
+  pathstr0='';
+end
 
+pathstr=[pathstr0 '\RCWA-MATLAB-module\'];
+
+if ~exist(pathstr,'dir') && isempty(pathstr0)
+  pathstr=['.\' pathstr];
+end
+if ~exist(pathstr,'dir')
+  pathstr=['..\' pathstr];
+end
+
+if exist(pathstr,'dir')
+  addpath([pathstr 'material\']);
+  addpath([pathstr 'RCWA\']);
+  addpath([pathstr 'shape\']);
+else
+  pathstr=nan;
+end
+
+end
---
+++ example-playground/RCWAupdatePATH.m
@@ -0,0 +1,1 @@
+../RCWAupdatePATH.m
